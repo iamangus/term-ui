e9d32f933a6e0e2aa90270302dc87734e268f191
Angoo <turnbull.angus@gmail.com>
2025-07-16 05:55:00 -0500

    refactor(deps): remove unused uuid import from main.go

--- src/main.go
+++ src/main.go
@@ -18,7 +18,6 @@ import (
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/google/uuid"
 
 	"terminal/internal/auth"
 	"terminal/internal/config"
